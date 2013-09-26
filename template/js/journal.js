44017a836ffb5868915e08c456413fd625cce24a
Kevin Ott <supercodingmonkey@gmail.com>
2013-09-26 18:20:11 -0400

    Start work on a general template
    The template should use the styles from 2.1.1 and some of the code from
    3.1.1

---
+++ template/js/journal.js
@@ -0,0 +1,14 @@
+/* Journal Controller
+ * 
+ * The journal controller is used to setup and handle Journals
+ * For an example of a Journal Assignment see SE 3.1.1
+ */
+var Journal = (function($) {
+	var solutions;
+	
+	return {
+		'setSolutions': function() {
+			
+		}
+	};
+})(jQuery);
